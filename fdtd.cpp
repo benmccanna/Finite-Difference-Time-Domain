742f7769612acc4b8581d5f47cff35f948f6dc1b
Ben McCanna <bendpmac@gmail.com>
2016-01-13 20:18:17 +0000

    reformatted code, removed info folder

--- fdtd.cpp
+++ fdtd.cpp
@@ -9,58 +9,62 @@
 
 using namespace std;
 
-const int outputInterval = 1; // how often (in frames) to record the state of the simulation
+// How often (in frames) to record the state of the simulation
+const int outputInterval = 1;
 const double courant = 1.0;
 const int defaultSource = 150;
 
-void simulate(Scenario &scene, int duration, int outputInterval, string basesname);
-void outputField(vector<double> &F, ofstream &output);
+void simulate(Scenario& scene,
+              int duration,
+              int outputInterval,
+              string basesname);
+void outputField(vector<double>& F, ofstream& output);
 
 int main() {
   int size, dur;
-  
+
   size = 201;
   dur = 500;
-  
+
   // Scenario basic;
   Scenario basic;
   basic.size = size;
-  basic.sourceNode = size/4;
+  basic.sourceNode = size / 4;
   simulate(basic, dur, outputInterval, "output/basic");
-  
+
   size = 201;
   dur = 1000;
 
   Standing standingWaves;
   standingWaves.size = size;
-  
-  for(int harmonic = 1; harmonic < 2; harmonic++) {
+
+  for (int harmonic = 1; harmonic < 2; harmonic++) {
     standingWaves.sourceNode = floor(size * 1 / (harmonic * 2));
     standingWaves.frequency = 0.5 * harmonic / double(size - 1);
-    simulate(standingWaves, dur, outputInterval, "output/standing-waves" + to_string(harmonic));
+    simulate(standingWaves, dur, outputInterval,
+             "output/standing-waves" + to_string(harmonic));
   }
-  
+
   size = 201;
   dur = 500;
 
-  class : public TotalScattered,
-          public Dielectric,
-          public AdvectionABC
-          {} incident;
+  class : public TotalScattered, public Dielectric, public AdvectionABC {
+  } incident;
   incident.size = size;
-  incident.sourceNode = size/4;
+  incident.sourceNode = size / 4;
   incident.dLeft = 100;
   incident.dRight = size - 1;
   incident.dielectricPermittivity = 9.0;
-  simulate(incident, dur, outputInterval, "output/Incident-on-perfect-dielectric");
-  
+  simulate(incident, dur, outputInterval,
+           "output/Incident-on-perfect-dielectric");
+
   class : public LossLayer,
           public TotalScattered,
           public Dielectric,
-          public AdvectionABC
-    {} lossyDielectric;
+          public AdvectionABC {
+  } lossyDielectric;
   lossyDielectric.size = size;
-  lossyDielectric.sourceNode = size/4;
+  lossyDielectric.sourceNode = size / 4;
   lossyDielectric.dLeft = 100;
   lossyDielectric.dRight = size - 1;
   lossyDielectric.lLeft = 180;
@@ -69,26 +73,29 @@ int main() {
   lossyDielectric.dielectricPermeability = 3.0;
   lossyDielectric.electricLoss = 0.02;
   lossyDielectric.magneticLoss = 0.08;
-  simulate(lossyDielectric, dur, outputInterval, "output/Dielectric-with-lossy-region");
-  
-  lossyDielectric.magneticLoss = 0.02;
-  simulate(lossyDielectric, dur, outputInterval, "output/Dielectric-with-lossy-region-matched");
+  simulate(lossyDielectric, dur, outputInterval,
+           "output/Dielectric-with-lossy-region");
 
+  lossyDielectric.magneticLoss = 0.02;
+  simulate(lossyDielectric, dur, outputInterval,
+           "output/Dielectric-with-lossy-region-matched");
 }
 
-/* Runs simulations and saves the results to files. Takes a `Scenario` which 
+/* Runs simulations and saves the results to files. Takes a `Scenario` which
 describes how to run the simulation; `duration`, the number of steps to
 simulate; `outputInterval`, the frequency with which to write the state of
 the simulation to our output files; and `basename`, the base file name to
 write the results of the simulation to. */
-void simulate(Scenario &scene, int duration, int outputInterval, string basename) {
-  
-  cout << "Running simulation " << basename  << "... " << endl;
-  
+void simulate(Scenario& scene,
+              int duration,
+              int outputInterval,
+              string basename) {
+  cout << "Running simulation " << basename << "... " << endl;
+
   /* Initialise the scene and make sure it doesn't have state left over from a
   previous simulation */
   scene.init();
-  
+
   // Open output files for the field arrays
   string filename;
   ofstream outE, outH;
@@ -96,32 +103,31 @@ void simulate(Scenario &scene, int duration, int outputInterval, string basename
   outE.open(filename.c_str());
   filename = basename + "-(Hx).dat";
   outH.open(filename.c_str());
-  
+
   // The actual simulation:
-  for(int tIndex = 0; tIndex < duration; tIndex++) {
+  for (int tIndex = 0; tIndex < duration; tIndex++) {
     // Step the simulation forward
     scene.step(tIndex);
-    
+
     // Output rows of data to our two output files every `outputInterval` steps
-    if(tIndex % outputInterval == 0) {
+    if (tIndex % outputInterval == 0) {
       outputField(scene.Ey, outE);
       outputField(scene.Hx, outH);
     }
   }
-  
+
   // Close our two output files
   outE.close();
   outH.close();
-  
+
   cout << "Done." << endl;
 }
 
 // Output a row `F` to file `output`
-void outputField(vector<double> &F, ofstream &output) {
+void outputField(vector<double>& F, ofstream& output) {
   int size = F.size();
-  for(int zIndex = 0; zIndex < size; zIndex++) {
+  for (int zIndex = 0; zIndex < size; zIndex++) {
     output << F[zIndex] << "\t";
   }
   output << endl;
-  
 }
--- scenario.cpp
+++ scenario.cpp
@@ -8,293 +8,268 @@
 
 using namespace std;
 
-const double imp0 = 377.0;    // Impedance of free space
-const double pi = 4.0*atan(1);
+const double imp0 = 377.0;  // Impedance of free space
+const double pi = 4.0 * atan(1);
 
 /* The Scenario class defines a minimal simulation, including the Yee algorithm
 for updating fields in time. We inherit from this class to define more complex
 simulations. */
 class Scenario {
-  public:
-    int size;           // Size of the space being simulated
-    int sourceNode;                   // Location of the source node
-    double sourcePeak = 30.0;         // Time of the source peak
-    double cour = 1.0;                // Courant number, determines how fast waves travel
-    vector<double> Ey, Hx;            // Electric and magnetic field arrays
-    
-    /* Init makes sure the scenario is ready to be run - it should also make
-    sure there's no state left over from previous simulations. By default, the
-    only state is in the two fields. */
-    virtual void init() {
-      /* Initialise fields to zero */
-      Ey = vector<double> (size, 0.0);
-      Hx = vector<double> (size, 0.0);
-      checkVars();
-    }
-    
-    /* The step function moves the simulation forward one step. */
-    void step(int tIndex) {
-      updateFields(tIndex);
-    }
+ public:
+  int size;                  // Size of the space being simulated
+  int sourceNode;            // Location of the source node
+  double sourcePeak = 30.0;  // Time of the source peak
+  double cour = 1.0;         // Courant number, determines how fast waves travel
+  vector<double> Ey, Hx;     // Electric and magnetic field arrays
 
-  protected:
-    virtual void checkVars() {
-      /* Some boundary conditions fail if an additive source is on or next to the 
-      boundary */
-      while(sourceNode < 2 || sourceNode > size - 2) {
-        cout << "Set field source position - any integer from 2 to " << size - 2
-          << endl;
-        cin >> sourceNode;
-      }
-      checkDielectric();
-      checkLossLayer();
-    }
-    virtual void checkDielectric() {}
-    virtual void checkLossLayer() {}
-    
-    /* Update the interior fields according to the Yee algorithm */
-    virtual void updateHx() {
-      double coeff, loss;
-      /* Magnetic field constant at rightmost node */
-      for(int zIndex = 0; zIndex < size - 1; zIndex++) {
-        loss = hLoss(zIndex);
-        Hx[zIndex] *= (1.0 - loss) / (1.0 + loss);
-        coeff = cour / imp0 / permeability(zIndex) / (1.0 + loss);
-        Hx[zIndex] += coeff * (Ey[zIndex + 1] - Ey[zIndex]);
-      }
-    }
-    virtual void updateEy() {
-      double coeff, loss;
-      /* Electric field constant at leftmost node */
-      for(int zIndex = 1; zIndex < size; zIndex++) {
-        loss = eLoss(zIndex);
-        Ey[zIndex] *= (1.0 - loss) / (1.0 + loss);
-        coeff = cour * imp0 / permittivity(zIndex) / (1.0 + loss);
-        Ey[zIndex] += coeff * (Hx[zIndex] - Hx[zIndex - 1]);
-      }
-    }
+  /* Init makes sure the scenario is ready to be run - it should also make
+  sure there's no state left over from previous simulations. By default, the
+  only state is in the two fields. */
+  virtual void init() {
+    /* Initialise fields to zero */
+    Ey = vector<double>(size, 0.0);
+    Hx = vector<double>(size, 0.0);
+    checkVars();
+  }
 
-    /* Field excited at specified node */
-    virtual double source(int tIndex) {
-      double srcT = double(tIndex) - sourcePeak;
-      return exp(-srcT*srcT / 100.0);
-    }
-    virtual void updateSource(int tIndex) {
-      Ey[sourceNode] += source(tIndex);
-    }
-    
-    virtual void abcValues() {}
-    virtual void tfsfCorrection(int tIndex) {}
-    virtual void abcHx() {}
-    virtual void abcEy() {}
-  
-    /* This function is a wrapper for all the possible field update functions
-    required by various scenarios, allowing derived classes to avoid conflicting
-    inheritance */
-    virtual void updateFields(int tIndex) {
-      abcValues();
-      updateHx();
-      tfsfCorrection(tIndex);
-      abcHx();
-      updateEy();
-      abcEy();
-      updateSource(tIndex);
-    }
+  /* The step function moves the simulation forward one step. */
+  void step(int tIndex) { updateFields(tIndex); }
 
-    /* Relative permittivity and permeability of material */
-    virtual double permittivity(int zIndex) {
-      return 1.0;
+ protected:
+  virtual void checkVars() {
+    /* Some boundary conditions fail if an additive source is on or next to the
+    boundary */
+    while (sourceNode < 2 || sourceNode > size - 2) {
+      cout << "Set field source position - any integer from 2 to " << size - 2
+           << endl;
+      cin >> sourceNode;
     }
-    virtual double permeability(int zIndex) {
-      return 1.0;
-    }
-    
-    /* Loss factor in the electric and magnetic fields due to electric and
-    magnetic conductivity respectively */
-    virtual double eLoss(int zIndex) {
-      return 0.0;
-    }
-    virtual double hLoss(int zIndex) {
-      return 0.0;
-    }
+    checkDielectric();
+    checkLossLayer();
+  }
+  virtual void checkDielectric() {}
+  virtual void checkLossLayer() {}
 
+  /* Update the interior fields according to the Yee algorithm */
+  virtual void updateHx() {
+    double coeff, loss;
+    /* Magnetic field constant at rightmost node */
+    for (int zIndex = 0; zIndex < size - 1; zIndex++) {
+      loss = hLoss(zIndex);
+      Hx[zIndex] *= (1.0 - loss) / (1.0 + loss);
+      coeff = cour / imp0 / permeability(zIndex) / (1.0 + loss);
+      Hx[zIndex] += coeff * (Ey[zIndex + 1] - Ey[zIndex]);
+    }
+  }
+  virtual void updateEy() {
+    double coeff, loss;
+    /* Electric field constant at leftmost node */
+    for (int zIndex = 1; zIndex < size; zIndex++) {
+      loss = eLoss(zIndex);
+      Ey[zIndex] *= (1.0 - loss) / (1.0 + loss);
+      coeff = cour * imp0 / permittivity(zIndex) / (1.0 + loss);
+      Ey[zIndex] += coeff * (Hx[zIndex] - Hx[zIndex - 1]);
+    }
+  }
+
+  /* Field excited at specified node */
+  virtual double source(int tIndex) {
+    double srcT = double(tIndex) - sourcePeak;
+    return exp(-srcT * srcT / 100.0);
+  }
+  virtual void updateSource(int tIndex) { Ey[sourceNode] += source(tIndex); }
+
+  virtual void abcValues() {}
+  virtual void tfsfCorrection(int tIndex) {}
+  virtual void abcHx() {}
+  virtual void abcEy() {}
+
+  /* This function is a wrapper for all the possible field update functions
+  required by various scenarios, allowing derived classes to avoid conflicting
+  inheritance */
+  virtual void updateFields(int tIndex) {
+    abcValues();
+    updateHx();
+    tfsfCorrection(tIndex);
+    abcHx();
+    updateEy();
+    abcEy();
+    updateSource(tIndex);
+  }
+
+  /* Relative permittivity and permeability of material */
+  virtual double permittivity(int zIndex) { return 1.0; }
+  virtual double permeability(int zIndex) { return 1.0; }
+
+  /* Loss factor in the electric and magnetic fields due to electric and
+  magnetic conductivity respectively */
+  virtual double eLoss(int zIndex) { return 0.0; }
+  virtual double hLoss(int zIndex) { return 0.0; }
 };
 
-class Standing: virtual public Scenario{
-  public:
-    void init() {
-      Ey = vector<double> (size, 0.0);
-      Hx = vector<double> (size - 1, 0.0);
-      checkVars();
+class Standing : virtual public Scenario {
+ public:
+  void init() {
+    Ey = vector<double>(size, 0.0);
+    Hx = vector<double>(size - 1, 0.0);
+    checkVars();
+  }
+  double frequency;
+  double source(int tIndex) {
+    double t = double(tIndex);
+    return sin(2.0 * pi * frequency * t);
+  }
+
+ protected:
+  void updateHx() {
+    double coeff, loss;
+    for (int zIndex = 0; zIndex < size - 1; zIndex++) {
+      loss = hLoss(zIndex);
+      Hx[zIndex] *= (1.0 - loss) / (1.0 + loss);
+      coeff = cour / imp0 / permeability(zIndex) / (1.0 + loss);
+      Hx[zIndex] += coeff * (Ey[zIndex + 1] - Ey[zIndex]);
     }
-    double frequency;
-    double source(int tIndex) {
-      double t = double(tIndex);
-      return sin(2.0 * pi * frequency * t);
+  }
+  void updateEy() {
+    double coeff, loss;
+    /* Electric field constant at left and rightmost node */
+    for (int zIndex = 1; zIndex < size - 1; zIndex++) {
+      loss = eLoss(zIndex);
+      Ey[zIndex] *= (1.0 - loss) / (1.0 + loss);
+      coeff = cour * imp0 / permittivity(zIndex) / (1.0 + loss);
+      Ey[zIndex] += coeff * (Hx[zIndex] - Hx[zIndex - 1]);
     }
-    
-  protected:
-    void updateHx() {
-      double coeff, loss;
-      for(int zIndex = 0; zIndex < size - 1; zIndex++) {
-        loss = hLoss(zIndex);
-        Hx[zIndex] *= (1.0 - loss) / (1.0 + loss);
-        coeff = cour / imp0 / permeability(zIndex) / (1.0 + loss);
-        Hx[zIndex] += coeff * (Ey[zIndex + 1] - Ey[zIndex]);
-      }
-    }
-    void updateEy() {
-      double coeff, loss;
-      /* Electric field constant at left and rightmost node */
-      for(int zIndex = 1; zIndex < size - 1; zIndex++) {
-        loss = eLoss(zIndex);
-        Ey[zIndex] *= (1.0 - loss) / (1.0 + loss);
-        coeff = cour * imp0 / permittivity(zIndex) / (1.0 + loss);
-        Ey[zIndex] += coeff * (Hx[zIndex] - Hx[zIndex - 1]);
-      }
-    }
-    
+  }
 };
 
 /* Adds a dielectric medium between dLeft and dRight */
-class Dielectric: virtual public Scenario{
-  public:
-    int dLeft, dRight;
-    double dielectricPermittivity = 2.0;
-    double dielectricPermeability = 1.0;
-  
-  protected:
-    void checkDielectric() {
-      while(dLeft < 0 || dLeft > size - 1) {
-        cout << "Set left edge of dielectric - any integer from 0 to "
-          << size - 1 << endl;
-        cin >> dLeft;
-      }
-      while(dRight < dLeft || dRight > size - 1) {
-        cout << "Set right edge of dielectric - any integer from " << dLeft
-          << " to " << size - 1 << endl;
-        cin >> dRight;
-      }
-    }
-    
-    double permittivity(int zIndex) {
-      if(zIndex < dLeft || zIndex > dRight) {
-        return 1.0;
-      }
-      return dielectricPermittivity;
-    }
-    double permeability(int zIndex) {
-      if(zIndex < dLeft || zIndex > dRight) {
-        return 1.0;
-      }
-      /* Averages the two permeability values on the boundaries so that the
-      interfaces are aligned with a particular (magnetic field) node */
-      if(zIndex == dLeft || zIndex == dRight) {
-        return 0.5 * (1.0 + dielectricPermeability);
-      }
-      return dielectricPermeability;
-    }
+class Dielectric : virtual public Scenario {
+ public:
+  int dLeft, dRight;
+  double dielectricPermittivity = 2.0;
+  double dielectricPermeability = 1.0;
 
+ protected:
+  void checkDielectric() {
+    while (dLeft < 0 || dLeft > size - 1) {
+      cout << "Set left edge of dielectric - any integer from 0 to " << size - 1
+           << endl;
+      cin >> dLeft;
+    }
+    while (dRight < dLeft || dRight > size - 1) {
+      cout << "Set right edge of dielectric - any integer from " << dLeft
+           << " to " << size - 1 << endl;
+      cin >> dRight;
+    }
+  }
+
+  double permittivity(int zIndex) {
+    if (zIndex < dLeft || zIndex > dRight) {
+      return 1.0;
+    }
+    return dielectricPermittivity;
+  }
+  double permeability(int zIndex) {
+    if (zIndex < dLeft || zIndex > dRight) {
+      return 1.0;
+    }
+    /* Averages the two permeability values on the boundaries so that the
+    interfaces are aligned with a particular (magnetic field) node */
+    if (zIndex == dLeft || zIndex == dRight) {
+      return 0.5 * (1.0 + dielectricPermeability);
+    }
+    return dielectricPermeability;
+  }
 };
 
 /* Adds a lossy region with non-zero electric and/or magnetic conductivity */
-class LossLayer: virtual public Scenario{
-  public:
-    int lLeft, lRight;
-    double electricLoss = 0.01;
-    double magneticLoss = 0.0;
-    
-  protected:
-    void checkLossLayer() {
-      while(lLeft < 0 || lLeft > size - 1) {
-        cout << "Set left edge of lossy region - any integer from 0 to "
-          << size - 1 << endl;
-        cin >> lLeft;
-      }
-      while(lRight < lLeft || lRight > size - 1) {
-        cout << "Set right edge of lossy region - any integer from " << lLeft
-          << " to " << size - 1 << endl;
-        cin >> lRight;
-      }
+class LossLayer : virtual public Scenario {
+ public:
+  int lLeft, lRight;
+  double electricLoss = 0.01;
+  double magneticLoss = 0.0;
+
+ protected:
+  void checkLossLayer() {
+    while (lLeft < 0 || lLeft > size - 1) {
+      cout << "Set left edge of lossy region - any integer from 0 to "
+           << size - 1 << endl;
+      cin >> lLeft;
     }
-    
-    double eLoss(int zIndex) {
-      if(zIndex < lLeft || zIndex > lRight) {
-        return 0.0;
-      }
-      return electricLoss;
+    while (lRight < lLeft || lRight > size - 1) {
+      cout << "Set right edge of lossy region - any integer from " << lLeft
+           << " to " << size - 1 << endl;
+      cin >> lRight;
     }
-    double hLoss(int zIndex) {
-      if(zIndex < lLeft || zIndex > lRight) {
-        return 0.0;
-      }
-      /* Again, to ensure the location of the interfaces is unambiguous */
-      if(zIndex == lLeft || zIndex == lRight) {
-        return 0.5*magneticLoss;
-      }
-      return magneticLoss;
+  }
+
+  double eLoss(int zIndex) {
+    if (zIndex < lLeft || zIndex > lRight) {
+      return 0.0;
     }
-  
+    return electricLoss;
+  }
+  double hLoss(int zIndex) {
+    if (zIndex < lLeft || zIndex > lRight) {
+      return 0.0;
+    }
+    /* Again, to ensure the location of the interfaces is unambiguous */
+    if (zIndex == lLeft || zIndex == lRight) {
+      return 0.5 * magneticLoss;
+    }
+    return magneticLoss;
+  }
 };
 
 /* Adds the simplest form of Absorbing boundary conditions (ABCs), allowing
 energy to leave the simulation */
-class AbsorbingBoundaries: virtual public Scenario {
+class AbsorbingBoundaries : virtual public Scenario {
   /* The previously fixed field values are now assigned to the past value of
   their nearest neighbour, eliminating reflection from the boundaries in
   free space scenarios */
-  protected:
-    virtual void abcValues() {
-      HxOldRight = Hx[size - 2];
-      EyOldLeft = Ey[1];
-    }
-    virtual void abcHx() {
-      Hx[size - 1] = HxOldRight;
-    }
-    virtual void abcEy() {
-      Ey[0] = EyOldLeft;
-    }
-    double HxOldRight, EyOldLeft;
-
+ protected:
+  virtual void abcValues() {
+    HxOldRight = Hx[size - 2];
+    EyOldLeft = Ey[1];
+  }
+  virtual void abcHx() { Hx[size - 1] = HxOldRight; }
+  virtual void abcEy() { Ey[0] = EyOldLeft; }
+  double HxOldRight, EyOldLeft;
 };
 
 /* Adds ABCs using a first order discretised version of the advection equation,
 letting less of the field reflect than in the naive case */
-class AdvectionABC: virtual public AbsorbingBoundaries {
-  public:
-    void init() {
-      double temp;
-      Scenario::init();
-      
-      temp = sqrt(permeability(0) * permittivity(0)) / cour;
-      coeffLeft = (1.0 - temp) / (1.0 + temp);
-      
-      temp = sqrt(permeability(size - 1) * permittivity(size - 1)) / cour;
-      coeffRight = (1.0 - temp) / (1.0 + temp);
-    }
+class AdvectionABC : virtual public AbsorbingBoundaries {
+ public:
+  void init() {
+    double temp;
+    Scenario::init();
 
-  protected:
-    /* Fields on the boundary are now updated according to their present values,
-    the local relative permittivity and permeability, and the past and present
-    values of their nearest neighbours */
-    void abcHx() {
-      Hx[size - 1] = HxOldRight + coeffRight * (Hx[size - 2] - Hx[size - 1]);
-    }
-    void abcEy() {
-      Ey[0] = EyOldLeft + coeffLeft * (Ey[1] - Ey[0]);
-    }
-    double coeffLeft, coeffRight;
+    temp = sqrt(permeability(0) * permittivity(0)) / cour;
+    coeffLeft = (1.0 - temp) / (1.0 + temp);
 
+    temp = sqrt(permeability(size - 1) * permittivity(size - 1)) / cour;
+    coeffRight = (1.0 - temp) / (1.0 + temp);
+  }
+
+ protected:
+  /* Fields on the boundary are now updated according to their present values,
+  the local relative permittivity and permeability, and the past and present
+  values of their nearest neighbours */
+  void abcHx() {
+    Hx[size - 1] = HxOldRight + coeffRight * (Hx[size - 2] - Hx[size - 1]);
+  }
+  void abcEy() { Ey[0] = EyOldLeft + coeffLeft * (Ey[1] - Ey[0]); }
+  double coeffLeft, coeffRight;
 };
 
 /* Replaces the source in the basic setup with an incident wave travelling
 rightwards from a point using a Total-Field/Scattered-Field boundary at that
 point */
-class TotalScattered: virtual public Scenario {
-  protected:
-    /* Corrects the update equation to make the source wave unidirecitonal */
-    void tfsfCorrection(int tIndex) {
-      Hx[sourceNode - 1] -= source(tIndex - 1) / imp0;
-    }
-  
+class TotalScattered : virtual public Scenario {
+ protected:
+  /* Corrects the update equation to make the source wave unidirecitonal */
+  void tfsfCorrection(int tIndex) {
+    Hx[sourceNode - 1] -= source(tIndex - 1) / imp0;
+  }
 };
